c1cdd27f68770112470c74a24edf0e536b1d60e1
Adam <61555703+akunsecured@users.noreply.github.com>
2021-02-24 18:52:27 +0100

    Hello PostgreSQL

--- src/main/java/hu/bme/aut/onlabpropertyhome/HelloWorldController.java
+++ src/main/java/hu/bme/aut/onlabpropertyhome/HelloWorldController.java
@@ -1,5 +1,8 @@
 package hu.bme.aut.onlabpropertyhome;
 
+import hu.bme.aut.onlabpropertyhome.model.User;
+import hu.bme.aut.onlabpropertyhome.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String helloWorld() {
+        User nu = new User();
+        nu.setEmail("asd");
+        nu.setName("asdd");
+
+        userRepository.save(nu);
+
         return "Hello World";
     }
 }

333872849abdb50dcf8812a665edacc27c028240
Adam <61555703+akunsecured@users.noreply.github.com>
2021-02-24 18:40:38 +0100

    Hello PostgreSQL

--- src/main/java/hu/bme/aut/onlabpropertyhome/model/UserRepository.java
+++ src/main/java/hu/bme/aut/onlabpropertyhome/model/UserRepository.java
@@ -1,7 +1,7 @@
 package hu.bme.aut.onlabpropertyhome.model;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }

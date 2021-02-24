5eef9d1c3efab13bf64ad73bbec93ded429f7975
Adam <61555703+akunsecured@users.noreply.github.com>
2021-02-24 16:59:45 +0100

    Hello World

---
+++ src/main/java/hu/bme/aut/onlabpropertyhome/OnlabPropertyhomeApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.onlabpropertyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlabPropertyhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlabPropertyhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bme/aut/onlabpropertyhome/OnlabPropertyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.onlabpropertyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlabPropertyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
